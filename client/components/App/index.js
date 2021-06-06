92236164c20c4d7f5482724f46a9cf1e76295ec3
Sergii <sergii.mykhailyk@gmail.com>
2021-06-06 21:05:42 +0300

    created project

---
+++ client/components/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './style';
+
+const App = () => (
+	<React.Fragment>
+		<div className={style.app}>Hello World</div>
+		<button onClick={(e) => alert('Hello You!')}>Say Hello Back!</button>
+	</React.Fragment>
+);
+
+export default App;
